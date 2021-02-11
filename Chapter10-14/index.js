c1a14377b67b6c70dbfc7765cfd7b54fa40ae7cb
TheAli711 <ali7112001@gmail.com>
2021-02-11 10:51:00 +0500

    imporved display msg in ch10-14assignment2

--- Chapter10-14/index.js
+++ Chapter10-14/index.js
@@ -15,11 +15,11 @@ if (op == "+" ) {
 
 }
 
-var course1 = parseInt(prompt("Enter course1"));
-var course2 = parseInt(prompt("Enter course2"));
-var course3 = parseInt(prompt("Enter course3"));
-var course4 = parseInt(prompt("Enter course4"));
-var course5 = parseInt(prompt("Enter course5"));
+var course1 = parseInt(prompt("Enter course1 marks"));
+var course2 = parseInt(prompt("Enter course2 marks"));
+var course3 = parseInt(prompt("Enter course3 marks"));
+var course4 = parseInt(prompt("Enter course4 marks"));
+var course5 = parseInt(prompt("Enter course5 marks"));
 var obtmarks = course1+course2+course3+course4+course5;
 var totalmarks = 500;
 alert("Obtained Marks = " + obtmarks);
