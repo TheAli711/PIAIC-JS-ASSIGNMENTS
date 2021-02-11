57703e9846966c4e31af6e613f2e72e4a14ffb2a
TheAli711 <ali7112001@gmail.com>
2021-02-11 12:14:52 +0500

    finished chapter 18-20

---
+++ Chapter18-20/index.js
@@ -0,0 +1,10 @@
+var courses=[];
+for (let index = 0; index < 5; index++) {
+    courses.push(prompt(`Enter course ${index+1}: `));
+}
+alert(courses);
+for (let index = 0; index < 5; index++) {
+    var input = prompt(courses[index],courses[index])
+    courses.splice(index,1,input);
+}
+alert(courses);
